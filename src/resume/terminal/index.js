2f805300b20d0f3d9934d3cf6d78ee6cc423cb5e
Brandon Wetzel <brandon.m.wetzel@gmail.com>
2024-01-14 13:01:03 -0500

    mobile font test

--- src/resume/terminal/index.js
+++ src/resume/terminal/index.js
@@ -26,50 +26,45 @@ export default class Terminal extends Component{
                         <div>-</div>
                     </div>
                 </div>
-                <div className="terminal-side"></div>
                 <div className="main-terminal">
-                <Typing 
-                    speed='80'
-                    string="cat BMW_Resume.txt"
-                    show_cursor="true"
-                    time_before_typing='800'
-                    end_cursor_time='2800'
-                    before="BMW@Sedna:~$ "
-                />
-                <div className="ASCII">
-                    {ASCII.lines.map((str, i) => {
-                        return (<Typing 
-                            speed="20"
-                            string={str}
-                            time_before_typing={2800+i*80}
-                            end_cursor_time="000"
-                            before=""
-                            key={i}
-                        />)
-                    })}
+                        <Typing 
+                            speed='80'
+                            string="cat BMW_Resume.txt"
+                            show_cursor="true"
+                            time_before_typing='800'
+                            end_cursor_time='2800'
+                            before="BMW@Sedna:~$ "
+                        />
+                    <div className="ASCII">
+                        {ASCII.lines.map((str, i) => {
+                            return (<Typing 
+                                speed="20"
+                                string={str}
+                                time_before_typing={2800+i*80}
+                                end_cursor_time="000"
+                                before=""
+                                key={i}
+                            />)
+                        })}
+                        
+                    </div>
+                    <Typing 
+                        speed="20"
+                        string="   "
+                        time_before_typing="4400"
+                        end_cursor_time="000"
+                        before=""
+                    />
+                    <Typing 
+                        speed="20"
+                        string="about me section"
+                        time_before_typing="4400"
+                        end_cursor_time="000"
+                        before=""
+                    />
                 </div>
-                </div>
-                <div className="terminal-side"></div>
-                <div className="terminal-bottom"></div>
             </div>
             
         )
     }
 }
-
-// <Typing 
-//                         speed='20'
-//                         string="hello world this is my resume"
-//                         show_cursor='false'
-//                         time_before_typing='0'
-//                         end_cursor_time='0'
-//                     />
-                    
-//                     <Typing 
-//                         speed='200' 
-//                         string="cat resume.txt" 
-//                         show_cursor="true" 
-//                         time_before_typing='1200'
-//                         end_cursor_time='3000'
-//                         before="BMW@Sedna$&nbsp; > "
-//                     />
